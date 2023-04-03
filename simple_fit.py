57064964349173559237c945efb224ea002141a2
Benjamin Eriksson <j.benjamin.eriksson@gmail.com>
2023-04-03 08:16:38 +0100

    Now reads DT and scatter TOF components directly instead of performing matrix multiplication of energy spectra with detector response function.

--- simple_fit.py
+++ simple_fit.py
@@ -79,7 +79,10 @@ def main(start_params, temp_path, out_file, dat_file, verbose=False):
     tofor.fit.data_xlim = (20, 80)
 
     # Set fit components
-    rmf.set_components(out_file)
+        # Set components
+    components = rmf.set_components('input_files/specs/bt_td_spec.json', 
+                                    'input_files/specs/scatter_spec.json', 
+                                    -0.7)
 
     # Start feed parameter fitting procedure
     # --------------------------------------
@@ -99,7 +102,7 @@ def main(start_params, temp_path, out_file, dat_file, verbose=False):
     """
     exe = 'fortran/run_fortran'
     popt = sp.optimize.minimize(rmf.fit_function, p0, options={'eps': 0.025},
-                                args=(exe, norm_p0, out_file,
+                                args=(exe, norm_p0, components, out_file,
                                       temp_path, verbose))
 
     return popt
